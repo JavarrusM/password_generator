6d9b6727b05a554b5c154b039075cd744fd13ce2
JavarrusM <jdavonm@gmail.com>
2022-11-25 16:14:56 -0500

    update: about page and links in templates

--- generator/views.py
+++ generator/views.py
@@ -6,6 +6,9 @@ import random
 def home(request):
     return render(request, 'generator/home.html')
 
+def about(request):
+    return render(request, 'generator/about.html')
+
 def password(request):
 
     thepassword = ''
